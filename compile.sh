8b251bd5512f920d5a7027f07065bab687b43c79
Vincenzo Mantova <v.l.mantova@leeds.ac.uk>
2025-09-23 17:20:34 +0100

    release v1.2.0
    - The release text includes direct links to each download and a link to
      the workflow run page on GitHub.
    - Error messages are highlighted in red in the workflow run page and are
      included prominently in the release text.
    - The workflow summary includes an abridged version of the release text.
    - Breaking change: custom values of `AUX_DIR` are ignored. This action
      sets its own value of `AUX_DIR` in order to compile the files in a
      temporary folder outside of the workspace.
    - Breaking change: the aux directory is now uploaded as job artifact
      rather than being attached to the release. This is essentially
      equivalent to the previous behaviour, except that artifacts normally
      expire after 90 days.

--- compile.sh
+++ compile.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-restartToken="restart-commands-$RANDOM$RANDOM"
-
-echo "::add-matcher::$GITHUB_ACTION_PATH/bookml.json"
-
 # input validation
 if ! [[ $TIMEOUT_MINUTES =~ ^[0-9]+$ ]] ; then
   workflow="${GITHUB_WORKFLOW_REF%@*}"
   workflow=".github/workflows/${workflow#*/.github/workflows/}"
   workflow_lineno="$(sed -n '/timeout-minutes:/{=;q;}' < "$workflow")"
-  echo "::error file=$workflow,line=${workflow_lineno:-1},title=Invalid timeout-minutes value::The argument timeout-minutes must be a positive integer."
+  echo "::error file=$workflow,line=${workflow_lineno:-1},title=Invalid timeout-minutes value::The value '$TIMEOUT_MINUTES' of timeout-minutes is not a positive integer."
+  echo 'outcome=invalid' >> "$GITHUB_OUTPUT"
   exit 1
 fi
 
+### Download BookML image
 case $SCHEME in
-  full) SCHEME= ;;
-  *) SCHEME="-$SCHEME" ;;
+  full) scheme= ;;
+  *) scheme="-$SCHEME" ;;
 esac
-
-IMAGE="ghcr.io/vlmantova/bookml$SCHEME:$VERSION"
+IMAGE="ghcr.io/vlmantova/bookml$scheme:$VERSION"
 
 echo "::group::Downloading BookML image \`$IMAGE\`"
 docker pull "$IMAGE"
@@ -28,48 +25,51 @@ if [[ $ret != 0 ]] ; then
   workflow=".github/workflows/${workflow#*/.github/workflows/}"
   workflow_lineno="$(sed -n '/scheme:/{=;q;}' < "$workflow")"
   workflow_lineno="${workflow_lineno:-$(sed -n '/version:/{=;q;}' < "$workflow")}"
-  echo "::error file=$workflow,line=${workflow_lineno:-1},title=Could not download Docker image::Could not download Docker image $IMAGE. Check if version and scheme are valid."
+  echo "::error file=$workflow,line=${workflow_lineno:-1},title=Could not download Docker image::Could not download Docker image $IMAGE. Check if version '$VERSION' and scheme '$SCHEME' are valid."
+  echo 'outcome=invalid' >> "$GITHUB_OUTPUT"
   exit "$ret"
 fi
 echo "::endgroup::"
+### end Download BookML image
 
-# TODO: parallel build
-echo "::group::Compiling using BookML image \`$IMAGE\`"
+### Compile with BookML image
+echo "::add-matcher::$GITHUB_ACTION_PATH/bookml.json"
+restartToken="restart-commands-$RANDOM$RANDOM"
 echo "::stop-commands::$restartToken"
 
+# TODO: parallel build
 docker run --rm --interactive=true --volume="$GITHUB_WORKSPACE":/source \
+  --volume="$RUNNER_TEMP/auxdir":/auxdir --volume="$GITHUB_OUTPUT":/github-output\
   --env=REPLACE_BOOKML="$REPLACE_BOOKML" --env=TIMEOUT_MINUTES="$TIMEOUT_MINUTES" \
-  --env=GITHUB_OUTPUT="$GITHUB_OUTPUT" --entrypoint /bin/bash "$IMAGE" -s <<'EOF'
+  --entrypoint /bin/bash "$IMAGE" -s <<'EOF'
 if [[ $REPLACE_BOOKML == true ]] ; then
   /run-bookml update || echo '::error title=Could not replace the bookml/ folder::Could not replace the bookml/ folder.'
 fi
 
 export max_print_line=10000
 
-timeout "$TIMEOUT_MINUTES"m /run-bookml -k all 2>&1 | tee .git/bookml-report
+timeout "$TIMEOUT_MINUTES"m /run-bookml -k all AUX_DIR=/auxdir 2>&1 | tee /auxdir/bookml-report
 
 case "${PIPESTATUS[0]}" in
-  124|137) outcome=cancelled
-    echo "::error title=Compiling cancelled::Increase \`timeout-minutes\` to allow more time." ;;
+  124|137) outcome=timeout
+    echo "::error title=Compiling timed out::Increase \`timeout-minutes\` to allow more time." ;;
   0) outcome=success ;;
-  *) outcome=failed ;;
+  *) outcome=failure ;;
 esac
 
-/run-bookml aux-zip
-
-targets="$(grep '^ Targets: ' < .git/bookml-report | head -n 1 | sed -E -e 's/^.*:\s*|(\s| )*$//g')"
+targets="$(grep '^ Targets: ' < /auxdir/bookml-report | head -n 1 | sed -E -e 's/^.*:\s*|(\s| )*$//g')"
 outputs="$(ls -C --width=0 $targets 2>/dev/null || :)"
 
-echo "outcome=$outcome" >> .git/github-output
-echo "targets=$targets" >> .git/github-output
-echo "outputs=$outputs" >> .git/github-output
+echo "outcome=$outcome" >> /github-output
+echo "targets=$targets" >> /github-output
+echo "outputs=$outputs" >> /github-output
 EOF
 
 echo "::$restartToken::"
-
-echo "::endgroup::"
 echo "::remove-matcher owner=bookml-latex-errors::"
 echo "::remove-matcher owner=bookml-latexml-errors::"
 echo "::remove-matcher owner=bookml-latexml-warnings::"
 
-cat .git/github-output >> "$GITHUB_OUTPUT"
+grep -q '^outcome=success$' "$GITHUB_OUTPUT"
+
+### end Compile with BookML image
--- release.sh
+++ release.sh
@@ -1,128 +1,135 @@
 #!/usr/bin/env bash
 
-TARGETS="${TARGETS// /, }"
-
-tag="bookml-$GITHUB_RUN_NUMBER-$GITHUB_RUN_ATTEMPT"
-downUrl="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/releases/download/$tag"
-
-# Downloads section
-downloads=$'\n'"### Downloads"
-for output in $OUTPUTS ; do
-  downloads+=$'\n'"- [$output]($downUrl/$output)"
-done
-
-auxzip="$(ls AUX.*.zip 2>/dev/null || : | head -n 1)"
-downloads+=$'\n'"- [$auxzip]($downUrl/$auxzip)"
+bookml_report="$RUNNER_TEMP"/auxdir/bookml-report
+workflow_report="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/attempts/$GITHUB_RUN_ATTEMPT"
+consult="Consult the [workflow report]($workflow_report)${AUX_URL:+ and the logs in the [aux directory]($AUX_URL)} for more information."
 
 # Header
 case $OUTCOME in
-  cancelled)
-    header=$'\n'"Compiling outputs $TARGETS has been cancelled."
-    if [[ -n $OUTPUTS ]] ; then
-      header+=$'\n'"Only ${OUTPUTS// /, } have been compiled."
-    else
-      header+=$'\nNo outputs have been compiled.'
-    fi
-    header+=$'\nIncrease `timeout-minutes` to allow more time.'
-    title='cancelled' ;;
+  invalid)
+    header="**The configuration of your GitHub workflow seems invalid.**"
+    header+=$'\nCheck that the values of `timeout-minutes`, `scheme`, `version` are valid.'
+    outcome=invalid ;;
   success)
-    if [[ -z $TARGETS ]] ; then
-      header=$'\nNo outputs have been built.'
-      header+=$'\nPlease check that that .tex files containing `\documentclass` exist in the top folder and that their filenames have no spaces.'
-      header+=$'\n'"Consult the [AUX file]($downUrl/$auxzip) for more information."
-    else
-      header=$'\n'"All outputs $TARGETS have been compiled successfully."
-    fi
-    title='successful' ;;
+    outcome=successful ;;
+  timeout)
+    header="**Compiling outputs $TARGETS has timed out.**"
+    header+=$'\nIncrease `timeout-minutes` to allow more time.'
+    header+=$'\n'"$consult"
+    outcome='timed out' ;;
+  cancelled)
+    header="**The workflow was cancelled while compiling $TARGETS.**"
+    header+=$'\n'"This may happen, for instance, if the workflow runs for more than 360 minutes. $consult"
+    outcome=cancelled ;;
   *)
-    if [[ -n $OUTPUTS ]] ; then
-      header=$'\n'"Only ${OUTPUTS// /, } have been compiled successfully, out of intended targets $TARGETS."
-    else
-      header=$'\n'"No outputs have been compiled, out of intended targets $TARGETS."
-    fi
-    header+=$'\n'"Consult the [AUX file]($downUrl/$auxzip) for more information."
-    title='failed' ;;
+    header="**An error has occurred.**"
+    header+=$'\n'"$consult"
+    outcome=failed ;;
 esac
 
+if [[ ! -f "$bookml_report" ]] ; then
+  header+=$'\n'"**BookML did not run.**"
+elif [[ -z $TARGETS ]] ; then
+  header+=$'\n**BookML did not try to compile any file.**'
+  header+=$'\nPlease check that .tex files containing `\documentclass` exist in the top folder and that their filenames have no spaces.'
+elif [[ -z $OUTPUTS ]] ; then
+  header+=$'\n'"**No outputs have been compiled, out of intended targets ${TARGETS// /, }.**"
+else
+  read -r -a outputs <<< "$OUTPUTS"
+  read -r -a targets <<< "$TARGETS"
+  if [[ ${#outputs[@]} < ${#targets[@]} ]] ; then
+    header+=$'\n'"**Only ${OUTPUTS// /, } have been compiled, out of intended targets ${TARGETS// /, }.**"
+  else
+    header+=$'\n'"**All outputs ${TARGETS// /, } have been compiled.**"
+  fi
+fi
+
+echo "$header" >> "$GITHUB_STEP_SUMMARY"
+
 # Error messages
-ERRORS="$(grep -E '^([^:]*):([0-9]+):\s*(.*)$|^(Error|Fatal):([^:]+:.*) at ([^;]+);( from)?( line ([0-9]+))?( col ([0-9]+))?( to line [0-9]+( col [0-9]+)?)?$' < .git/bookml-report || :)"
-
-if [[ -n $ERRORS ]] ; then
-  errorMessages=$'\n\n### Error messages\n'
-  while IFS= read -r error ; do
-   errorMessages+=$'\n- '"$error"
-  done <<< "$ERRORS"
+if [[ -e "$bookml_report" ]] ; then
+  errors="$(grep -E '^([^:]*):([0-9]+):\s*(.*)$|^(Error|Fatal):([^:]+:.*) at ([^;]+);( from)?( line ([0-9]+))?( col ([0-9]+))?( to line [0-9]+( col [0-9]+)?)?$' < "$bookml_report" || :)"
+  errors="${errors%$'\n'}"
+  if [[ -n $errors ]] ; then
+    errorMessages=$'### Error messages\n\n- '"${errors//$'\n'/$'\n'- }"
+  fi
 fi
 
-# Release title
-title="$title compiling: $MESSAGE"
 
-# Messages
-# note: the body of a GitHub release cannot exceed 125000 bytes
-SIZE="$(wc -c < .git/bookml-report)"
-MAX=$((124000 - ${#header} - ${#downloads} - ${#errorMessages}))
-if [[ $SIZE -gt $MAX ]] ; then
-  read -r -d '' messages<<EOF
-### Summarised output messages
-<pre><code>
-$(head -n 5 .git/bookml-report)
-
-[...]
-
-$(tail -n 5 .git/bookml-report)
-</code></pre>
-
-### Truncated output messages
-<details><summary><b>Click to show the last $MAX characters of the output messages</b></summary>
-<pre><code>
-[TRUNCATED] $(tail -c $MAX .git/bookml-report)
-</code></pre></details>
-EOF
-
-elif [[ $(wc -l < .git/bookml-report) -lt 20 ]] ; then
-  read -r -d '' messages<<EOF
-### Full output messages
-<pre><code>
-$(cat .git/bookml-report)
-</code></pre>
-EOF
-
-else
-  read -r -d '' messages<<EOF
-### Summarised output messages
-<pre><code>
-$(head -n 5 .git/bookml-report)
-
-[...]
-
-$(tail -n 5 .git/bookml-report)
-</code></pre>
-
-### Full output messages
-<details><summary><b>Click to show the full output messages</b></summary>
-<pre><code>
-$(cat .git/bookml-report)
-</code></pre></details>
-EOF
+if [[ -n $AUX_URL ]] ; then
+  aux_download=$'\n'"- [aux directory]($AUX_URL) including all outputs and other compilation products (this link will expire after the number of retention days specified in your repository settings)"
 fi
 
-# Release notes
-read -r -d '' notes <<EOF
-$header
-$downloads
-$errorMessages
+if [[ $RELEASE == true ]] ; then
+  # Release title
+  title="$outcome: $MESSAGE"
 
-Full [workflow report]($GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/attempts/$GITHUB_RUN_ATTEMPT).
-$messages
-EOF
+  tag="bookml-$GITHUB_RUN_NUMBER-$GITHUB_RUN_ATTEMPT"
+  downUrl="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/releases/download/$tag"
 
-output="$(gh release create "$tag" --target="$GITHUB_REF_NAME" --repo="$GITHUB_REPOSITORY" --title="$title" --notes="$notes" $OUTPUTS "$auxzip" 2>&1)"
-ret="$?"
-if [[ $ret != 0 ]] ; then
-  echo "::error title=GitHub release failed::${output//$'\n'/%0A}"
-  echo "$header"$'\n'"$errorMessages" >> "$GITHUB_STEP_SUMMARY"
-  exit "$ret"
+  if [[ -e $bookml_report ]] ; then
+    # Messages
+    # note: the body of a GitHub release cannot exceed 125000 bytes
+    if [[ $(wc -l < "$bookml_report") -lt 20 ]] ; then
+      messages='### Full output messages'
+      messages+=$'\n<pre><code>\n'
+      messages+="$(cat "$bookml_report")"
+      messages+=$'</code></pre>\n'
+    else
+      SIZE="$(wc -c < "$bookml_report")"
+      MAX=$((124000 - ${#header} - ${#downloads} - ${#errorMessages}))
+      messages='### Summarised output messages'
+      messages+=$'\n<pre><code>\n'
+      messages+="$(head -n 5 "$bookml_report")"
+      messages+=$'\n[...]\n\n'
+      messages+="$(tail -n 5 "$bookml_report")"
+      messages+=$'</code></pre>\n'
+      if [[ $SIZE -gt $MAX ]] ; then
+        messages+=$'\n### Truncated output messages'
+        messages+=$'\n'"<details><summary><b>Click to show the last $MAX characters of the output messages</b></summary>"
+        messages+=$'\n<pre><code>\n'
+        messages+="[TRUNCATED] $(tail -c $MAX "$bookml_report")"
+        messages+=$'\n'"</code></pre></details>"
+      else
+        messages+=$'\n### Full output messages'
+        messages+=$'\n'"<details><summary><b>Click to show the full output messages</b></summary>"
+        messages+=$'\n<pre><code>\n'
+        messages+="$(cat "$bookml_report")"
+        messages+=$'\n'"</code></pre></details>"
+      fi
+    fi
+  fi
+
+  # Add links to each output in Downloads section
+  for output in $OUTPUTS ; do
+    downloads+=$'\n'"- [$output]($downUrl/$output)"
+  done
+
+  downloads+="$aux_download"
+
+  # Release notes
+  notes="$header"
+  [[ -z $downloads ]] || notes+=$'\n\n### Downloads\n\n'"$downloads"
+  [[ -z $errorMessages ]] || notes+=$'\n\n'"$errorMessages"
+  notes+=$'\n\n'"Full [workflow report]($workflow_report)."
+  notes+=$'\n\n'"$messages"
+
+  release="$(gh release create "$tag" --target="$GITHUB_REF_NAME" --repo="$GITHUB_REPOSITORY" --title="$title" --notes="$notes" $OUTPUTS 2>&1)"
+  ret="$?"
+
+  if [[ $ret != 0 ]] ; then
+    echo "::error title=GitHub release failed::${release//$'\n'/%0A}"
+    [[ -z $aux_download ]] || echo $'\n### Downloads\n\n'"$aux_download" >> "$GITHUB_STEP_SUMMARY"
+    [[ -z $errorMessages ]] || echo $'\n'"$errorMessages" >> "$GITHUB_STEP_SUMMARY"
+    exit "$ret"
+  else
+    echo "$release"
+    echo "release-url=$release" >> "$GITHUB_OUTPUT"
+    [[ -z $downloads ]] || echo $'\n### Downloads\n'"$downloads" >> "$GITHUB_STEP_SUMMARY"
+    echo $'\n'"[Release page]($release)." >> "$GITHUB_STEP_SUMMARY"
+    [[ -z $errorMessages ]] || echo $'\n'"$errorMessages" >> "$GITHUB_STEP_SUMMARY"
+  fi
 else
-  echo "$output"
-  echo "$header"$'\n'"$downloads"$'\n\n'"[Release page]($output)."$'\n'"$errorMessages" >> "$GITHUB_STEP_SUMMARY"
+  [[ -z $aux_download ]] || echo $'\n### Downloads\n'"$aux_download" >> "$GITHUB_STEP_SUMMARY"
+  [[ -z $errorMessages ]] || echo $'\n'"$errorMessages" >> "$GITHUB_STEP_SUMMARY"
 fi
